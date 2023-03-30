080a7b88e9921fa350928a685da018d425257111
Erika dos Reis <86990284+erikadsrs@users.noreply.github.com>
2023-03-30 18:31:41 -0300

    Aprimora a Classe App para Create Sticker

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -28,11 +31,26 @@ public class App {
         //System.out.println(listaFilmes.size());
         //System.out.println(listaFilmes.get(0));
 
+        //cria diretorio para saida
+        var diretorio = new File("FigurinhasFinal/" );
+        diretorio.mkdir();
+
         //EXIBIÇÃO
         for (Map<String,String> filme : listaFilmes) {
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
+        
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            CreateSticker.criaSticker(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
         }
     }
 }
